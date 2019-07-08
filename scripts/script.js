dc25f188700217ca21cb7d1f1bf0a2ec8afac028
Maciej <maciej.zelazo@protonmail.com>
2019-07-08 13:29:16 +0200

    added gallery of people

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,40 @@ window.onload = function () {
     const date = document.getElementById("date");
     date.insertAdjacentText("beforeend", today);
 }
+
+
+// init variables
+const imagesTotal = 10;
+let currentImage = 1;
+
+function previousArrow() {
+    $('#previousArrow').addClass("button-inactive");
+    $('#nextArrow').removeClass("button-inactive");
+    $('#gallery-thumbnails').removeClass("slide-right");
+    $('#nextArrowIcon').removeClass('arrow-inactive');
+    $('#previousArrowIcon').addClass('arrow-inactive');
+}
+
+function nextArrow() {
+    $('#previousArrow').removeClass("button-inactive");
+    $('#nextArrow').addClass("button-inactive");
+    $('#gallery-thumbnails').addClass("slide-right");
+    $('#gallery-thumbnails').addClass("slide-right");
+    $('#nextArrowIcon').addClass('arrow-inactive');
+    $('#previousArrowIcon').removeClass('arrow-inactive');
+}
+// ===================
+
+
+// BULLETS CODE
+function changeImage(imageNumber) {
+    $('figure#person' + currentImage).removeClass("big-image-active");
+    $('figure#person' + currentImage).addClass("big-image-hidden");
+
+    $('#thumbnails-image' + currentImage + ' img').removeClass("active");
+    currentImage = imageNumber;
+    $('figure#person' + imageNumber).removeClass("big-image-hidden");
+    $('figure#person' + imageNumber).addClass("big-image-active");
+    $('#thumbnails-image' + imageNumber + ' img').addClass("active");
+}
+// ===================
